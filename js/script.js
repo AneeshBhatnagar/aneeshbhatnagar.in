6db08db6bdfcf3ba2db24a20c51e12b3d963575e
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-07-19 17:16:17 -0400

    Handled Responsive Nature of Homepage

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ $(document).ready(function(){
 		$(this).children("div.menu-image").children("img.circle-image").css("transform","rotate(90deg)");
 	}, function(){
 		$(this).children("div.menu-image").children("img.circle-image").css("transform","rotate(0deg)");
-	})
+	});
+	assignNavHeight();
+});
+
+
+function assignNavHeight(){
+	//Height for homepage navigation
+	var width = $(".menu-item-inner .circle-image").width();
+	$(".menu-item-inner").css("height",width);
+}
+
+
+/*Event Handlers*/
+$(window).resize(function(){
+	assignNavHeight();
 });
