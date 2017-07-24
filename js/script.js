cf244e236f542f879af33828fda88a5a819c4497
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-07-24 00:06:26 -0400

    Created About Page

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
 		$(this).children("div.menu-image").children("img.circle-image").css("transform","rotate(0deg)");
 	});
 	assignNavHeight();
+	$(".skills-images img").tooltipster({
+		position:"top",
+		delay:120,
+		theme:"tooltipster-borderless"
+	});
+	$(".menu-item-inner").tooltipster({
+		position:"right",
+		delay: 100,
+		theme: "tooltipster-borderless"
+	});
 });
 
 
