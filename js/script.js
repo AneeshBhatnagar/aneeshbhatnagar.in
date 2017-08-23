6de99f6eb343b9a64a19f56a6e907535a2e5f30d
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-08-23 12:46:33 -0400

    Created Responsive Menu and Contact Form

--- js/script.js
+++ js/script.js
@@ -15,6 +15,67 @@ $(document).ready(function(){
 		delay: 100,
 		theme: "tooltipster-borderless"
 	});
+
+	/* Contact Form Handling */
+
+	$("#contact-form").on('submit', function() {
+	$('#contact-form .success').addClass('hidden');
+	$('#contact-form .failure').addClass('hidden');
+	var name = $('#contact-form #name');
+	var email = $('#contact-form #email');
+	var message = $('#contact-form #message');
+	var url = $(this).attr('action');
+	var data = $(this).serialize();
+
+	console.log(data);
+	var errors = 0;
+
+	if (name.val() == '') {
+		name.addClass('error');
+		errors = 1;
+	}
+
+	if (email.val() == '') {
+		email.addClass('error');
+		errors = 1;
+	}
+
+	if (!validateEmail(email.val())) {
+		email.addClass('error');
+		errors = 1;
+	}
+
+	if (message.val() == '') {
+		message.addClass('error');
+		errors = 1;
+	}
+
+	if (errors == 0) {
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: data,
+			success: function(response) {
+				console.log("Response from AJAX" + response);
+				if (response == 1) {
+					$('#contact-form').each(function() {
+						this.reset();
+					});
+					grecaptcha.reset();
+					$('#contact-form .success').removeClass('hidden');
+				} else{
+					$('#contact-form .failure').removeClass('hidden');
+				}
+
+			},
+		});
+	}
+
+
+
+	return false;
+});
+	
 });
 
 
@@ -22,6 +83,8 @@ function assignNavHeight(){
 	//Height for homepage navigation
 	var width = $(".menu-item-inner .circle-image").width();
 	$(".menu-item-inner").css("height",width);
+	nav_position = $("#side-navigation").position().top;
+	content_position = $("#main-content").position().left;
 }
 
 
@@ -29,3 +92,20 @@ function assignNavHeight(){
 $(window).resize(function(){
 	assignNavHeight();
 });
+
+$(window).scroll(function(){
+	var windowPos=$(window).scrollTop();
+
+	if(windowPos >= nav_position){
+		$("#side-navigation").addClass("sticky-side-navigation");
+		$("#main-content").css("left",content_position);
+	}else{
+		$("#side-navigation").removeClass("sticky-side-navigation");
+		$("#main-content").css("left","0px");
+	}
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
